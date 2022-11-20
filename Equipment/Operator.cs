8ffdd49491f826bcbdb2863c87692317147506a8
Clueless <luke.morrell.19@gmail.com>
2022-11-19 21:28:50 -0500

    test commit
    should show that i added a console.writeline that does nothing

--- Equipment/Operator.cs
+++ Equipment/Operator.cs
@@ -8,5 +8,6 @@ namespace Equipment
 {
     internal class Operator
     {
+
     }
 }
--- Equipment/Program.cs
+++ Equipment/Program.cs
@@ -12,6 +12,8 @@ namespace Equipment
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            Console.WriteLine("This is a test");
+
         }
     }
 }
